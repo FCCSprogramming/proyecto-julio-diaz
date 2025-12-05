4d8666625ba7c1cf101771f699bf378714d84227
juliodc25 <julio.diaz.c@uni.pe>
2025-12-05 11:08:10 -0500

    estableciendo main

---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "persona.h"
+#include "curso.h"
+#include "sistema.h"
+#include "estudiante.h"
+
+
+
+
+using namespace std;
+
+int main() {
+    int opcion;
+    while (opcion!=11) {
+    cout<<"===Sistema de Gestion Academica UNI==="<<endl;
+    cout<<"1)Registrar Estudiante"<<endl;
+    cout<<"2)Registrar a Curso"<<endl;
+    cout<<"3)Inscribir a Curso"<<endl;
+    cout<<"4)Registrar nota parcial"<<endl;
+    cout<<"5)Calcular promedio y generar reporte"<<endl;
+    cout<<"6)Almacenar datos de estudiantes aprobados"<<endl;
+    cout<<"7)Guardar base de datos"<<endl;
+    cout<<"8)Cargar base de datos"<<endl;
+    cout<<"9)Acceso directo a registros"<<endl;
+    cout<<"10)Listar datos registrados"<<endl;
+    cout<<"11)Salir"<<endl;
+    cout<<"Seleccione una opcion: ";
+    cin>>opcion;
+    switch (opcion)
+    {
+    case 1:
+        
+        break;
+    case 2:
+        
+        break;
+    case 3:
+        
+        break;
+    case 4: 
+        
+        break;        
+    case 5:
+        
+        break;
+    case 6:
+        
+        break;
+    case 7:
+        
+        break;
+    case 8:
+        
+        break;
+    case 9:
+        
+        break;
+    case 10:
+        
+        break;
+    case 11:
+        cout<<"Saliendo del sistema..."<<endl;
+        return 0;
+    default:
+        break;
+    }
+    }
+
+
+    
+    return 0;
+}
