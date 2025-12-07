59105ec24488acb6f85ac125320fc11fa87ffce2
juliodc25 <julio.diaz.c@uni.pe>
2025-12-07 14:53:02 -0500

    desarrollo

--- src/curso.cpp
+++ src/curso.cpp
@@ -1,1 +1,22 @@
 #include "curso.h"
+
+Curso::Curso(){ 
+}
+void Curso::setNombreCurso(const std::string& nombreCurso){
+    this->nombreCurso = nombreCurso;
+}
+std::string Curso::getNombreCurso() const {
+    return nombreCurso;
+}
+void Curso::setCodigoCurso(const std::string& codigoCurso){
+    this->codigoCurso = codigoCurso;
+}
+std::string Curso::getCodigoCurso() const {
+    return codigoCurso;
+}
+void Curso::setCreditos(int creditos){
+    this->creditos = creditos;
+}
+int Curso::getCreditos() const {
+    return creditos;
+}
