12c9ee650ddb32015869a8e14a0170cab999ba53
juliodc25 <julio.diaz.c@uni.pe>
2025-12-07 14:51:59 -0500

    desarrollo

--- src/persona.cpp
+++ src/persona.cpp
@@ -1,2 +1,23 @@
 #include "persona.h"
 
+Persona::Persona(){ 
+}
+
+Persona::~Persona(){ 
+}
+
+void Persona::setNombre(const std::string& nombre){
+    this->nombre = nombre;
+}
+
+std::string Persona::getNombre() const {
+    return nombre;
+}
+
+void Persona::setEdad(int edad){
+    this->edad = edad;
+}
+
+int Persona::getEdad() const {
+    return edad;
+}
