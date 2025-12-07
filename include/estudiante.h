8d94877121dffbc16ddd8540f5cfb708b9009e33
juliodc25 <julio.diaz.c@uni.pe>
2025-12-07 14:52:43 -0500

    desarrollo

--- include/estudiante.h
+++ include/estudiante.h
@@ -1,5 +1,24 @@
 #ifndef ESTUDIANTE_H
 #define ESTUDIANTE_H
+#include <string>
+#include "persona.h"
+#include "curso.h"
+
+class Estudiante : public Persona {
+    private:
+        std::string codigo;
+        int cantCursos;
+        Curso* cursos=new Curso[cantCursos];
+    public:
+        Estudiante();
+        Curso* ptr = cursos;
+        void mostrarInfo() override;
+        float calcularDesempenio() const override;
+        void setCodigo(const std::string&);
+        std::string getCodigo() const;
+        void setCantCursos(int);
+        int getCantCursos()const;
+};
 
 
 
