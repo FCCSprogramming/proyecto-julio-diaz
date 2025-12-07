a73dd3271060f37cc8993f363c6220011c33dada
juliodc25 <julio.diaz.c@uni.pe>
2025-12-07 14:53:25 -0500

    agregue desarrollo en los archivos

--- include/curso.h
+++ include/curso.h
@@ -1,7 +1,22 @@
 #ifndef CURSO_H
 #define CURSO_H
+#include <string>
 
 
+class Curso {
+private:
+   std:: string nombreCurso;
+   std:: string codigoCurso;
+   int creditos;
+public:
+   Curso();
+   void setNombreCurso(const std::string&);
+   std::string getNombreCurso() const;
+   void setCodigoCurso(const std::string&);
+   std::string getCodigoCurso() const;
+   void setCreditos(int);
+   int getCreditos() const;    
+};
 
 
 
