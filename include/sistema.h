3ab9c0accf2cd05c0866ddd90bd3c5b35141347f
juliodc25 <julio.diaz.c@uni.pe>
2025-12-07 14:53:10 -0500

    desarrollo

--- include/sistema.h
+++ include/sistema.h
@@ -2,5 +2,18 @@
 #define SISTEMA_H
 
 
+class Sistema {
+public:
+    void registrarEstudiante();
+    void registrarCurso();
+    void inscribirCurso();
+    void registrarNotaParcial();
+    void calcularPromedio_generarReporte();
+    void almacenarDatosEstudiantesAprobados();
+    void guardarBaseDeDatos();
+    void cargarBaseDeDatos();
+    void accesoDirectoRegistros();
+    void listarDatosRegistrados();
+};
 
 #endif
