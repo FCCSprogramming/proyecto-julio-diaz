03d62d8aeaf1f572236e159f94e6524bd412e66a
juliodc25 <julio.diaz.c@uni.pe>
2025-12-10 00:14:03 -0500

    desarrollo(curso.h)

--- include/curso.h
+++ include/curso.h
@@ -8,14 +8,18 @@ private:
    std:: string nombreCurso;
    std:: string codigoCurso;
    int creditos;
+   float notaParcial;
 public:
    Curso();
+   ~Curso();
    void setNombreCurso(const std::string&);
    std::string getNombreCurso() const;
    void setCodigoCurso(const std::string&);
    std::string getCodigoCurso() const;
    void setCreditos(int);
-   int getCreditos() const;    
+   int getCreditos() const;
+   void setNotaParcial(float);
+   float getNotaParcial() const;    
 };
 
 
