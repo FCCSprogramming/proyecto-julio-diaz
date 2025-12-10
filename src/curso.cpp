12b7b4109180ad4953f0537d855e1675140a2459
juliodc25 <julio.diaz.c@uni.pe>
2025-12-10 00:13:48 -0500

    desarrollo(curso.cpp)

--- src/curso.cpp
+++ src/curso.cpp
@@ -2,6 +2,8 @@
 
 Curso::Curso(){ 
 }
+Curso::~Curso(){ 
+}
 void Curso::setNombreCurso(const std::string& nombreCurso){
     this->nombreCurso = nombreCurso;
 }
@@ -19,4 +21,10 @@ void Curso::setCreditos(int creditos){
 }
 int Curso::getCreditos() const {
     return creditos;
+}
+void Curso::setNotaParcial(float notaParcial){
+    this->notaParcial = notaParcial;
+}
+float Curso::getNotaParcial() const {
+    return notaParcial;
 }
