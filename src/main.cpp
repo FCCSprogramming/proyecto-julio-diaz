ef57620b1ff62e29eb325e20031add1dbe9d74ba
juliodc25 <julio.diaz.c@uni.pe>
2025-12-16 00:53:00 -0500

    implementacion de archivos,funciones y llamada a funciones

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,20 @@
 #include "curso.h"
 #include "sistema.h"
 #include "estudiante.h"
-
-
+#include <fstream>
 
 
 using namespace std;
 
+fstream archivo("C:\\Users\\Personal\\Documents\\GitHub\\proyecto-julio-diaz\\estudiantes aprobados.txt");//generamos el archivo de texto 
+fstream archivoBin("C:\\Users\\Personal\\Documents\\GitHub\\proyecto-julio-diaz\\base de datos.bin");//generamos el archivo binario
+
 int main() {
-     const int numCursos=10;
+    const int numCursos=10;
     int contCursos=0;
-    Sistema sistema(20,nullptr);
-    Curso* cursos = new Curso[numCursos];
-    Curso* ptrCurso = cursos;
+    Sistema sistema(20,nullptr);//establecemos la maxima cantidad de estudiantes como 20 e inicialmente no hay estudiantes registrados
+    Curso* cursos = new Curso[numCursos];//arreglo dinamico de cursos(max 10)
+    Curso* ptrCurso1 = cursos;
     int opcion;
     while (opcion!=11) {
     cout<<"===Sistema de Gestion Academica UNI==="<<endl;
@@ -34,33 +36,33 @@ int main() {
     switch (opcion)
     {
     case 1:
-        sistema.registrarEstudiante();
+        sistema.registrarEstudiante();//va registrando estudiantes en el sistema,no es necesario parametros
         break;
     case 2:
-        if(contCursos==numCursos){
+        if(contCursos==numCursos){//usamos el contCursos para que deje de registrar cursos al llegar al maximo
             cout<<"Limite de cursos alcanzado."<<endl;
             break;
         }
-        sistema.registrarCurso(cursos,ptrCurso);
+        sistema.registrarCurso(cursos,ptrCurso1);
         contCursos++;
         break;
     case 3:
-        
+        sistema.inscribirCurso(ptrCurso1,contCursos);
         break;
     case 4: 
-        
+        sistema.registrarNotaParcial(ptrCurso1);
         break;        
     case 5:
-        
+        sistema.calcularPromedio_generarReporte(sistema,cursos,ptrCurso1,numCursos);
         break;
     case 6:
-        
+        sistema.almacenarDatosEstudiantesAprobados(sistema,cursos,ptrCurso1,numCursos,archivo);
         break;
     case 7:
-        
+        sistema.guardarBaseDeDatos(cursos,ptrCurso1,numCursos,archivoBin);
         break;
     case 8:
-        
+        sistema.cargarBaseDeDatos(archivoBin);
         break;
     case 9:
         
